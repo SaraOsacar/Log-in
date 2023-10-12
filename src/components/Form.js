5154e98eff53c7165b3f3c72ee665398a3a1463b
SaraOsacar <saraosacar@gmail.com>
2023-10-12 18:01:56 +0200

    Event name & password

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,25 @@
-import "../styles/Form.scss";
+import "../styles/Form.scss"; 
+import { useState } from "react";
 
 export function Form () {
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+
+
     return(
         <section>
             <h1 className="title">Login</h1>
 
             <form className="form">
-                <input type="text" />
-                <input type="password" />
+                <input type="text" 
+                placeholder="Name"
+                value={name}
+                onChange={e => setName(e.target.value)}
+                />
+                <input type="password" 
+                placeholder="Password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}/>
                 <button>Sign in</button>
             </form>
         </section>
