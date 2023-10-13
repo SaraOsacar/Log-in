6fc8ab28b07ca50426a230b795bc2bb1f7e2f6b9
SaraOsacar <saraosacar@gmail.com>
2023-10-13 12:48:29 +0200

    Error message (incomplete fields)

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,24 +4,39 @@ import { useState } from "react";
 export function Form () {
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState(false);
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        if(name === "" || password === "") {
+            setError(true)
+            return
+        }
+        setError(false)
+    }
 
 
-    return(
-        <section>
-            <h1 className="title">Login</h1>
+    return (
+      <section>
+        <h1 className="title">Login</h1>
 
-            <form className="form">
-                <input type="text" 
-                placeholder="Name"
-                value={name}
-                onChange={e => setName(e.target.value)}
-                />
-                <input type="password" 
-                placeholder="Password"
-                value={password}
-                onChange={e => setPassword(e.target.value)}/>
-                <button>Sign in</button>
-            </form>
-        </section>
-    )
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button>Sign in</button>
+        </form>
+        {error && <p className="errorMessage">All fields are required</p>}
+      </section>
+    );
     }
