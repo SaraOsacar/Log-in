ae69d99c3d331dc21452923468ec6c7a389a5216
SaraOsacar <saraosacar@gmail.com>
2023-10-19 15:26:10 +0200

    Render Home (username)

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,21 @@
 import '../styles/App.scss';
 import {Form} from './Form';
 import {Home} from './Home';
-import { useState} from 'react';
+import { useState } from 'react';
 
 function App() {
   
   const [user, setUser] = useState ([])
 
-
+ 
   return (
     <div className="App">
-     <Form setUser={setUser}/>
-     <Home />
+      {
+         ! user.length > 0
+        ? <Form setUser={setUser} />
+        :<Home user={user}/> 
+      }
+    
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import '../styles/Home.scss';
 
-export function Home() {
+export function Home({ user }) {
   return (
     <div>
       <h1 className="homeTitle">Welcome</h1>
+      <h2 className="userName">{user}</h2>
     </div>
   );
 }
